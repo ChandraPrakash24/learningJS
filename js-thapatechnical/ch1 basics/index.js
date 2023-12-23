aa817d0d80bcd5cd0a0b9e4d50dac2c2f2c47f0d
ChandraPrakash24 <2412prakas@gmail.com>
2023-12-23 21:18:23 +0530

    Learning Array Methods

--- js-thapatechnical/ch1 basics/index.js
+++ js-thapatechnical/ch1 basics/index.js
@@ -365,3 +365,376 @@
 // 7 spread operator
 // 8 rest operator
 
+
+//                     1 let and const
+
+// var --> functional scope   function(){...if{...}...}
+// let --> block scope   function(){  if{...}  }
+
+//                       2 templet String
+
+// console.log(`..${JS}..`);
+
+
+//                      3 default arguments
+
+
+// function mult(a,b=5){
+//     return a * b;
+// }
+// console.log(mult(3));
+    
+    
+
+//          6 arrow functions (once defined then call) ( IT SOES NOT SUPPORT 'this')
+
+// const sum = (a,b) => {
+//     return `sum is : ${a+b}`;
+// }
+
+// console.log(sum(1,2));
+
+//               OR
+
+// const sum = (a,b) => `the sum is ${a+b}`;
+// const sum = (a,b) => a+b;
+
+// console.log(sum(1,2));
+
+
+//****************************** ARRAYS *************************************
+
+// in js there is Array Class and Array are the prototypes of this class
+
+//  Array initilize
+
+// let myFriends = new Array;
+// myFriends = ['aju','kaju','baju','raju'];
+
+// ALSO
+
+// let arr = [
+//     'apple', 'bannan', 2, 80, true, ['a', 'b'], { key : 'value',anoKey : 'value2' },
+//     function () {
+//         console.log('This is a function');
+//     },
+//     undefined,
+//     null
+// ];
+
+
+// Accessing elements in the array
+// console.log(arr[0]);      // 'apple'
+// console.log(arr[5][1]);   // 'b'
+// console.log(arr[6].key);  // 'value'
+
+// // Calling the function stored in the array
+// arr[7]();  // Outputs: 'This is a function'
+
+
+// myFriends = ['aju','kaju','baju','raju'];
+
+//                               1 Traversal in an array
+
+// 1 For Loop:
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// 2 For...of Loop:
+// for (let elements of arr) {
+//     console.log(elements);
+// }
+
+//3 forEach Method:
+
+// arr.forEach(  element =>console.log(element)  );
+// arr.forEach(function(element,index,array){
+//  console.log(element + "----> index: " + index + " -----> " + array);
+// });
+// OUTPUT:-
+
+// apple----> index: 0 -----> apple, bannan, 2, 80, true, a, b, [object Object], function () {
+//     console.log('This is a function');
+// },,
+//     bannan----> index: 1 -----> apple, bannan, 2, 80, true, a, b, [object Object], function () {
+//         console.log('This is a function');
+//     },,
+//         2----> index: 2 -----> apple, bannan, 2, 80, true, a, b, [object Object], function () {
+//             console.log('This is a function');
+//         },,
+//             80----> index: 3 -----> apple, bannan, 2, 80, true, a, b, [object Object], function () {
+//                 console.log('This is a function');
+//             },,
+//                 true----> index: 4 -----> apple, bannan, 2, 80, true, a, b, [object Object], function () {
+//                     console.log('This is a function');
+//                 },,
+//                     a, b----> index: 5 -----> apple, bannan, 2, 80, true, a, b, [object Object], function () {
+//                         console.log('This is a function');
+//                     },,
+//                         [object Object]----> index: 6 -----> apple, bannan, 2, 80, true, a, b, [object Object], function () {
+//                             console.log('This is a function');
+//                         },,
+//                             function () {
+//                                 console.log('This is a function');
+//                             }----> index: 7 -----> apple, bannan, 2, 80, true, a, b, [object Object], function () {
+//                                 console.log('This is a function');
+//                             },,
+//                                 undefined----> index: 8 -----> apple, bannan, 2, 80, true, a, b, [object Object], function () {
+//                                     console.log('This is a function');
+//                                 },,
+//                                     null----> index: 9 -----> apple, bannan, 2, 80, true, a, b, [object Object], function () {
+//                                         console.log('This is a function');
+//                                     },,
+
+// 4 Map Method:
+
+// arr.map(function(element) {
+//     console.log(element);
+// });
+
+
+// 5 While Loop:
+// let i = 0;
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+//6 Do...while Loop:
+
+// let i = 0;
+// do {
+//     console.log(arr[i]);
+//     i++;
+// } while (i < arr.length);
+
+// 7 for...in loop:
+
+// for(let e in arr) console.log(e); // 0 1 2 .....9
+// for(let e in arr) console.log(arr[e]); 
+// OUTPUT: -
+// apple
+// bannan
+// 2
+// 80
+// true
+// [ 'a', 'b' ]
+// { key: 'value', anoKey: 'value2' }
+// [Function (anonymous)]
+// undefined
+// null
+
+
+//               2 Searching and Filtering
+
+// let complexArray = [
+//     'apple',
+//     'banana',
+//     2,
+//     80,
+//     true,
+//     ['a', 'b'],
+//     { key: 'value', anotherKey: 'value2' },
+//     function () {
+//         console.log('This is a function');
+//     },
+//     undefined,
+//     null,
+//     { fruits: ['apple', 'banana', 'orange'], numbers: [1, 2, 3] }
+// ];
+
+// Linear Search for a specific value
+// function linearSearch(array, target) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === target) {
+//             return i; // Return the index if the target is found
+//         }
+//     }
+//     return -1; // Return -1 if the target is not found
+// }
+
+// console.log('Linear Search for "banana":', linearSearch(complexArray, 'banana'));
+
+// ADD: array.lastIndexOf();
+
+// Array.includes() method for searching
+// console.log('Array.includes() for "80":', complexArray.includes(80));
+ 
+// Array.indexOf() method for searching
+// console.log('Array.indexOf() for "value2":', complexArray.indexOf({ key: 'value', anotherKey: 'value2' }));
+// console.log('Array.indexOf() for "80":', complexArray.indexOf(80));
+
+// Find index using findIndex() method for objects with specific property
+// let indexOfObject = complexArray.findIndex(item => item && item.anotherKey === 'value2');
+// console.log('Find index using findIndex():', indexOfObject);
+// let indexOfObject = complexArray.findIndex(item => item === 'banana');
+// console.log('Find index using findIndex():', indexOfObject);
+
+// Using filter() to find elements that meet a condition
+// let filteredArray = complexArray.filter(item => typeof item === 'object' && item !== null);
+// console.log('Filtered Array (objects only):', filteredArray);
+
+
+//      fealtring
+
+// 1 Filter by Type (e.g., Strings):
+
+// let stringArray = complexArray.filter(item => typeof item === 'string');
+// console.log('Filter by Type (Strings):', stringArray);
+
+
+// 2 Filter by Condition (e.g., Numbers greater than 10):
+
+
+// let numbersGreaterThan10 = complexArray.filter(item => typeof item === 'number' && item > 10);
+// console.log('Filter by Condition (Numbers > 10):', numbersGreaterThan10);
+
+
+// 3 Filter by Array Length (e.g., Arrays with more than 2 elements):
+
+// let arraysWithMoreThan2Elements = complexArray.filter(item => Array.isArray(item) && item.length > 2);
+// console.log('Filter by Array Length (> 2 elements):', arraysWithMoreThan2Elements);
+
+// 4 Filter by Object Property (e.g., Objects with a specific key):
+
+// let objectsWithKey = complexArray.filter(item => typeof item === 'object' && 'key' in item);
+// console.log('Filter by Object Property (has "key"):', objectsWithKey);
+
+// 5 Filter by Function Presence:
+
+// let functionsInArray = complexArray.filter(item => typeof item === 'function');
+// console.log('Filter by Function Presence:', functionsInArray);
+
+
+// 6 Filter out Null and Undefined:
+
+// let nonNullOrUndefinedValues = complexArray.filter(item => item !== null && item !== undefined);
+// console.log('Filter out Null and Undefined:', nonNullOrUndefinedValues);
+
+
+
+//                  CRUD in an Array
+
+// const animal = ['pigs','goats','sheep'];
+
+
+// 1 push ( it reaters the new length of the array )
+
+// const returenedCount = animal.push('dog','cow');
+// console.log(returenedCount);
+
+// console.log(animal);
+// for(e of animal) console.log(e);
+
+// OUTPUT: 
+// 5
+// [ 'pigs', 'goats', 'sheep', 'dog', 'cow' ]
+
+
+// 2 unshift (push at front) ( it reaters the new length of the array )
+
+// const count = animal.unshift('cat');
+// console.log(count);
+// console.log(animal);
+
+// OUTPUT: 
+// 6
+// [ 'cat', 'pigs', 'goats', 'sheep', 'dog', 'cow' ]
+
+// 3 pop ( return the poped element and changes the length of orignal array)
+
+// const lastElemOfPop = animal.pop();
+// console.log(lastElemOfPop); // cow
+// console.log(animal); // [ 'cat', 'pigs', 'goats', 'sheep', 'dog' ]
+
+// 4 shift (pop from front)
+// const frontEleOfPop = animal.shift();
+// console.log(frontEleOfPop); // cow
+// console.log(animal); // ['pigs', 'goats', 'sheep', 'dog' ]
+
+// ****************************CRUD*********************************
+
+// 4  SPLICE METHOD ()
+
+// its adds and remove elements from the array
+// it changes orignal array
+// it returns empty array or array of deleted elements
+
+// const months = ['Jan','march','April','June','July'];
+// console.log(months.length); // 5
+
+// add Dec to end of array. 
+// SYNTEX: array.splice( poisition, detelete element[0/1], 'String' )
+// const newMonth = months.splice(5,0,'Dec');
+// const newMonth = months.splice(months.length,0,'Dec');
+// console.log(newMonth); // []
+// console.log(months); // [ 'Jan', 'march', 'April', 'June', 'July', 'Dec' ]
+// console.log(months.length); // 6
+
+// Updatae 'march' to 'March: -
+
+// const isInclude = months.includes('march');
+// console.log(isInclude);
+// const indexOfMonth = months.indexOf('march');
+// console.log(indexOfMonth);
+// // const UpdateMonths = months.splice(1,1,'March');
+// const UpdateMonths = months.splice(indexOfMonth,1,'March');
+// console.log(UpdateMonths); // ['march']
+// console.log(months); // [ 'Jan', 'March', 'April', 'June', 'July', 'Dec' ]
+
+// delete 'June' from an array
+
+// const indexOfMonthToDelete = months.indexOf('June');
+
+// if(indexOfMonthToDelete != -1){
+//     const deletedMonth = months.splice(3,1);
+//     console.log(deletedMonth);
+// }
+
+// console.log(deletedMonth); // deletedMonthis not accessable out of scope
+// console.log(months);
+
+// OUTPUT: 
+// [ 'June' ]
+// [ 'Jan', 'march', 'April', 'July' ]
+
+
+// delete all the element after 2nd element
+
+// const indexOfSecEle = months.indexOf('April');
+
+// if( indexOfSecEle != -1){
+//     const deletedRest = months.splice(indexOfSecEle,Infinity);
+//     console.log(deletedRest);
+// }
+// console.log(months);
+// OUTPUT: 
+// deletedRest -->  [ 'April', 'June', 'July' ]
+// months --> [ 'Jan', 'march' ]
+
+// let myArray = ['apple', 'banana', 'orange', 'grape'];
+
+// // Remove 2 elements from the end
+// myArray.splice(-2);
+
+// console.log(myArray); // Output: ['apple', 'banana']
+
+
+/****************************************************************************/
+
+//                             Map()
+
+// Array.prototype.map()
+
+// let newArray = arr.map(callback(currentValue[,index[, array]])) {
+    // return element from newArray after executing somthing
+// }[, thisArg]);
+
+// 
+
+
+
+
+
+
