aa3abff179c4eb28b2289db8431463dc49978064
ChandraPrakash24 <2412prakas@gmail.com>
2024-04-30 22:37:55 +0530

    revising

--- zero_to_expert_js/index.js
+++ zero_to_expert_js/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //****************************** VALUES AND VARIABLES *************************************
 
 // Value: smallest unit in js
@@ -275,25 +277,215 @@
 
 /*********************** CODING CHALLENGE **************************/
 
-let dolphinsScore1 = 96;
-let dolphinsScore2 = 108;
-let dolphinsScore3 = 89;
+// let dolphinsScore1 = 96;
+// let dolphinsScore2 = 108;
+// let dolphinsScore3 = 89;
 
-let koalaScore1 = 88;
-let koalaScore2 = 91;
-let koalaScore3 = 89;
+// let koalaScore1 = 88;
+// let koalaScore2 = 91;
+// let koalaScore3 = 110;
 
-let dolphinsAverageScore = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3;
+// let dolphinsAverageScore = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3;
 
-let koalaAverageScore = (koalaScore1 + koalaScore2 + koalaScore3) / 3;
+// let koalaAverageScore = (koalaScore1 + koalaScore2 + koalaScore3) / 3;
 
-console.log(`Average score of the team Dolphin: ${dolphinsAverageScore}
-Average score of the team Koala: ${koalaAverageScore}`);
+// console.log(`Average score of the team Dolphin: ${dolphinsAverageScore}
+// Average score of the team Koala: ${koalaAverageScore}`);
 
-if (dolphinsAverageScore === koalaAverageScore) {
-    console.log(`ITS A DRAW`);
-} else if (dolphinsAverageScore >= koalaAverageScore) {
-    console.log(`DOLPHINS WINS`);
-} else {
-    console.log(`KOALA WINS`);
-}
+// if (dolphinsAverageScore === koalaAverageScore) {
+//     console.log(`ITS A DRAW`);
+// } else if (dolphinsAverageScore >= koalaAverageScore) {
+//     console.log(`DOLPHINS WINS`);
+// } else {
+//     console.log(`KOALA WINS`);
+// }
+
+
+/********************************** SWITCH STATEMENT ***********************************/
+
+
+// const day = 'sunday';
+
+// switch(day){
+//     case 'monday': // day === 'monday' (strict check)
+//         console.log('its a Monday');
+//         console.log('multi line can be executed in one switch case');
+//         break;
+//     case 'tuesday':
+//         console.log('its a tuesday');
+//         break;
+//     case 'wednesday':
+//     case 'thrusday':
+//     case 'friday':
+//         if(day === 'wednesday') console.log('its a wednesday');
+//         else if(day === 'thrusday') console.log('its a thrusday');
+//         else console.log('its a friday');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('its a weeknd');
+//         break;
+//     default:
+//         console.log('Enter valid day');
+// }
+
+// same above code using if else logic
+
+// let day = 'sunday';
+
+// if(day === 'monday'){
+//     console.log('its a monday');
+//     console.log('Multi line of code');
+// }
+// else if(day === 'tuesday') console.log('its a tuesday');
+// else if(day === 'wednesday' || day === 'thrusday' || day === 'friday'){
+//     if(day === 'wednesday') console.log('its a wednesday');
+//     else if (day === 'thrusday') console.log('its a thrusday');
+//     else console.log('its friday');
+// }
+// else if(day === 'saturday' || day === 'sunday') console.log('its a weeknd');
+// else console.log('enetr valid day');
+
+
+/********************* Statements and Expressions ******************************/
+
+// data.forEach(element =>  console.log(element));
+
+// here data.forEach(....); this is a statemnet tels what to do
+
+// and element =>  console.log(element) this is a expression produces a value
+
+/************************* Tarnery Operator(Conditionals) ****************************/
+
+// 10 > 5 ? console.log("true") : console.log("false"); // true
+
+// we use bove as :
+
+// const age = 22;
+// const isEligable = age >= 18 ? "vote" : "no vote"; // it just return
+// console.log(isEligable);
+
+//************************ CODING CHALLENEGE ************************/
+
+// let billAmount = 40;
+
+// let tipPer;
+// billAmount >= 50 && billAmount <= 300 ? tipPer = 15 : tipPer = 20; 
+//                OR
+// let tipPer = billAmount >= 50 && billAmount <= 300 ? 15 : 20; 
+
+// console.log((billAmount * tipPer)/100);
+
+// console.log(`The bill was ${billAmount}, the tip was ${(billAmount * tipPer)/100}, and the total value ${billAmount + ((billAmount * tipPer)/100)}`);
+
+
+/********************************* USE STRICT EXAMPLE ***********************************/
+
+// let hasDriversLicence = false;
+// const passTest = true;
+
+// if (passTest) hasDriverLicence = true;
+// if (hasDriversLicence) console.log('i can drive.');
+
+
+// const intefcase = 'audio';
+// const private = 24;
+
+
+/**************************** FUNCTIONS *********************************/
+
+// function logger() {
+//     console.log('Logger function is called / invoked / execute / runned');
+// }
+
+// logger();
+// console.log(logger); // [Function: logger]
+// console.log(logger()); // undefined
+// logger();
+// logger();
+// logger();
+// logger();
+
+
+// function fruidJuicer(fruit1, fruit2) {
+//     console.log(`fruit 1 is ${fruit1}, fruit 2 is ${fruit2}`);
+//     const juice = `here is your fruit juice of ${fruit1} and ${fruit2}`;
+//     return juice;
+// }
+
+// fruidJuicer("apple","bannana"); // fruit 1 is apple, fruit 2 is bannana
+// it just log this coz above function was colled which has log statement
+
+// it did not print that 'juice'that we have make inside that function beacouse it was returned by the function which was stored by the coller at the place whre it was colled to do that we have to first store that result and then print it like below
+
+
+// const getJuice = fruidJuicer("mango","orange"); 
+// console.log(getJuice); // here is your fruit juice of mango and orange
+
+
+
+
+
+
+// function fruitJuicer2(apple,bannana) { // parameter
+//     return `here is youre juice of ${apple} apple and ${bannana} bannana`;
+// }
+
+// console.log(fruitJuicer2(4,6)); // argument // here is youre juice of 4 and 6 bannana
+// const getJuiceNo = fruidJuicer2(2,2); // here is youre juice of 4 and 6 bannana
+// console.log(getJuiceNo); // here is youre juice of 2 and 2 bannana
+
+// FUN FACT : console.log(); is also a function (an built-in function) which take argument and print it
+
+/**************************** Function decleration and Function Expression **********************************/
+
+// function decleration
+// const age1 = calcAge1(2001);
+
+// function calcAge1(birthYear) {
+//     return 2024 - birthYear;
+// }
+
+
+// function expression 
+// const calcAge2 = function (birthYear) {
+    //     return 2024 - birthYear;
+    // }
+    
+    // this part is called Anynomous function
+    // function (birthYear) {
+        //     return 2024 - birthYear;
+        // }
+        
+        // const age2 = calcAge2(2001);
+        
+// console.log(age1, age2);
+
+
+
+
+/**************************** Arrow Function **********************************/
+
+// const calcAge3 = birthYear => 2024 - birthYear;
+//            OR
+// const calcAge3 = (birthYear) => {
+//     return 2024 - birthYear;
+// }
+
+// const age3 = calcAge3(2001);
+// console.log(age3);
+// console.log(calcAge3(2001));
+
+// for multi parameter and multi line og code
+
+// const retirementYear = (birthYear, firstName) => {
+//     // const age = 2024 - birthYear;
+//     const age = calcAge3(birthYear);
+//     const retirementAge = 65 - age;
+//     return `${firstName} will retire in ${retirementAge} years.`
+// }
+
+// console.log(retirementYear(2001,"cpgl"));
+// console.log(`what a time you have spend and ${retirementYear(2001,"cpgl")} `);
+
+
