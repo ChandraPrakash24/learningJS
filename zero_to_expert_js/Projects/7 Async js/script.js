85d18df0bc3481e0b7f6649f4cd23dbeaea2f345
ChandraPrakash24 <2412prakas@gmail.com>
2024-07-26 19:27:16 +0530

    Learning fetch

--- zero_to_expert_js/Projects/7 Async js/script.js
+++ zero_to_expert_js/Projects/7 Async js/script.js
@@ -96,14 +96,14 @@ setTimeout(()=>{
 
 // code hard to understand -> BAD code -> more bug -> less mantainablity
 
-*/
+
 
 // ----- Solution of callback hell using promises: -----
 
 // ----- Modern way of doing ajax call using fetch ----- 
 
-const data = fetch('https://restcountries.com/v3.1/name/bharat');
-console.log(data); // Promise {<pending>}
+// const data = fetch('https://restcountries.com/v3.1/name/bharat');
+// console.log(data); // Promise {<pending>}
 
 // An object is used as a placeholder for the future result of an asynchronous operation
 //         OR
@@ -149,24 +149,173 @@ console.log(data); // Promise {<pending>}
 // }
 
 // cleaner version:
+// const getCounteryData = name => {
+//     fetch(`https://restcountries.com/v3.1/name/${name}`)
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+// }
+
+// getCounteryData('bharat');
+// getCounteryData('bharat');
+
+// chaining promises:
+// chaining two sequential call:
+
+// const getCounteryData = name => {
+//     fetch(`https://restcountries.com/v3.1/name/${name}`)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//         // chaining
+//         console.log(data[0].borders?.[0])
+//         const neighbour = data[0].borders?.[0];
+//         console.log(neighbour);
+//         return neighbour;
+//     })
+//     .then(data2 => {
+//         console.log('data2 === neighbour',data2);
+//         return fetch(`https://restcountries.com/v3.1/alpha/${data2}`);
+//     })
+//     .then(neighbourResponse => {
+//         console.log(neighbourResponse);
+//         return neighbourResponse.json();
+//     })
+//     .then(neighbourData => console.log(neighbourData))
+// }
+
+// getCounteryData('bharat');
+
+// even cleaner version of then chaining looks messy
+
+// const getCounteryData = name => {
+//     fetch(`https://restcountries.com/v3.1/name/${name}`)
+//     .then(response => response.json())
+//     .then(data => data[0].borders?.[0]) // we are not doing console.log(data of first couentry here)
+//     .then(data2 => fetch(`https://restcountries.com/v3.1/alpha/${data2}`))
+//     .then(neighbourResponse => neighbourResponse.json())
+//     .then(neighbourData => console.log(neighbourData))
+// }
+
+// getCounteryData('bharat');
+
+// .then(data => data[0].borders?.[0]) // we are not doing console.log(data of first couentry here)
+// .then(data2 => fetch(`https://restcountries.com/v3.1/alpha/${data2}`))
+//                               ===
+// .then(data => fetch(`https://restcountries.com/v3.1/alpha/${data[0].borders?.[0]}`))
+
+
+// THEN HELL:-
+
+// .then
+//    .then
+//      .then.
+//         .then
+
+// FLAT THEN CHAIN:-
+
+// .then
+// .then
+// .then
+// .then
+
+
+// ---------- Error Handling ----------
+
+// const getCounteryData = name => {
+//     fetch(`https://restcountries.com/v3.1/name/${name}`)
+//     .then(response => response.json(), err => console.log('ERROR:',err))
+//     .then(data => data[0].borders?.[0]) // we are not doing console.log(data of first couentry here)
+//     .then(data2 => fetch(`https://restcountries.com/v3.1/alpha/${data2}`))
+//     .then(neighbourResponse =>{ neighbourResponse.json();
+//         throw Error('I was constructed via the "new" keyword!');
+//     })
+//     .then(neighbourData => console.log(neighbourData))
+//     // .then(neighbourData => console.log(neighbourData), err => console.log(err))
+//     // SYNTEX: .then(()=>{},(err)=>err) <- second colback for error in .then
+//     .catch(err => console.log(err)) // ALSO GLOBAL ERROR HANDLE USING .catch AT THE LAST OF .then CHAIN
+//     // catch also return promise then finally consume it
+//     .finally(()=>{
+//         // it always run no matter the state of promise (fullfiled or rejected)
+//         // use to hide loading spinner at the end
+//     });
+// }
+
+// getCounteryData('bharat');
+
+// ----------- throw new Error('message') -----------
+
 const getCounteryData = name => {
     fetch(`https://restcountries.com/v3.1/name/${name}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
+    .then(response => {
+        if(!response.ok) throw new Error(`No couentry with this name: ${response.message}`);
+        return response.json();
+    })
+    .then(data => data[0].borders?.[0]) // we are not doing console.log(data of first couentry here)
+    .then(data2 => fetch(`https://restcountries.com/v3.1/alpha/${data2}`))
+    .then(neighbourResponse =>neighbourResponse.json())
+    .then(neighbourData => console.log(neighbourData))
+    .catch(err => console.error('No country:',err))
+    .finally(()=>{
+        console.log('finaly');
+    });
 }
 
-getCounteryData('bharat');
+getCounteryData('utuy');
 
 
+// throw is ;ike break; or return;
+// throw also rejects promise (nullify it)
+// .catch is more suitable with throw new Error
+// .catch can handle all kinds of error (from fetch as well as from then) and even travelling error form previous then downwords
+// second collback of .then is used to handle error of that 'individual' then only
+
+//ex:
+// fetch('https://example.com/data')
+//   .then(response => {
+//     if (!response.ok) { throw new Error('Network response was not ok'); } <-- IMP: do by yourself
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('There was a problem with the fetch operation:', error);
+//   });
 
 
+// REVERSE GEOCODE URL:_
 
+// https://nominatim.openstreetmap.org/reverse?format=json&lat=54.9824031826&lon=9.2833114795&zoom=18&addressdetails=1
+// https://nominatim.openstreetmap.org/reverse?format=json&lat=19.0760&lon=72.8777
+// https://geocode.xyz/51.50354,-0.12768?geoit=xml
+// https://www.geonames.org/findNearbyPlaceName?lat=40.65&lng=-73.78
 
+*/
 
+// CODING CHALLENGE:-
+// which couentry i am base on lattitude and longitude
 
+const whereAmI = function(lat,lon) {
+    // fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lon=${lon}&zoom=18&addressdetails=1`) // BAD REQUEST
+    fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=18&addressdetails=1`)
+    .then((response) => {
+        if(!response?.ok) throw new Error(`Wrong Lattitude or longitude values ${response.ststus}`);
+        return response.json();
+    })
+    // (err)=>console.log("ERR:",err))
+    .then((data) => {
+        console.log(data?.address?.country);
+    })
+    .catch((err) => {
+        console.error('Error is :- ',err);
+    })
+    .finally(() => {
+        console.log('FINNALY');
+    });
+}
 
-
-
+whereAmI(54.9824031826,9.2833114795); //denmark
+// whereAmI(789.9824031826,9.2833114795); //undefine
 
 
 
